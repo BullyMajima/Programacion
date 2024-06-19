e5cc99984b068c475210ff5ee263b9662c8635f4
BullyMajima <CAMILOCASTILLO478@GMAIL.COM>
2024-06-18 21:35:43 -0400

    segundo cambio

--- sumanumeros.py
+++ sumanumeros.py
@@ -1,4 +1,5 @@
-num1=float(input("ingrese un numero 1"))
-num2=float(input("ingrese un numero 2"))
+num1=int(input("ingrese un numero 1: "))
+num2=int(input("ingrese un numero 2: "))
 
-sumar=num1+num2print("la suma de dos numeros es:",sumar)
+sumar=num1+num2
+print("la suma de dos numeros es:",sumar)
